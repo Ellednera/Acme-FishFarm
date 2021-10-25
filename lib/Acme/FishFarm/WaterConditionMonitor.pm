a1f93ad8753f6b27350c5e272aa6cb852b231f04
Ellednera <raphael.jongjj@gmail.com>
2021-10-25 21:22:54 +0800

    Water Condition Monitor half-done, tests pass

--- lib/Acme/FishFarm/WaterConditionMonitor.pm
+++ lib/Acme/FishFarm/WaterConditionMonitor.pm
@@ -3,6 +3,7 @@ package Acme::FishFarm::WaterConditionMonitor;
 use 5.006;
 use strict;
 use warnings;
+use Carp;
 
 =head1 NAME
 
@@ -21,9 +22,6 @@ our $VERSION = '0.01';
 
     use Acme::FishFarm::WaterConditionMonitor;
 
-    my $foo = Acme::FishFarm::WaterConditionMonitor->new();
-    ...
-
 =head1 EXPORT
 
 None
@@ -40,16 +38,16 @@ The C<%sensors> included are:
 
 =item pH
 
-Optional. The default is [6.5, 7.5]. I might be wrong
+Optional. The default threshold range is C<[6.5, 7.5]> and the default pH is C<7.0>.
 
 This will set the threshold value of the water pH. Please pass in an array reference to this key
 in the form of C<[min_pH, max_pH]>
 
-The values are in the range of 1-14.
+The values are in the range of C<1-14>.
 
 =item temperature
 
-Optional. The default is [20, 25] degree celcius. I might be wrong.
+Optional. The default threshold range is C<[20, 25]> degree celcius and the default temprature is C<25>.
 
 This will set the threshold value of the water temperature. Please pass in an array reference to this key
 in the form of C<[min_temperature, max_temperature]>
@@ -58,15 +56,15 @@ The ranges of values are between 0 and 50 degree B<celciuls>
 
 =item DO
 
-Optional. The default is 5 mg/L.
+Optional. The default threshold is C<5 mg/L> and the default DO is to C<8 mg/L>.
 
 This will set the threshold value of the DO in the water.
 
-The range of values are between 0 and 10 mg/L
+The range of values are between 0 and 10 mg/L.
 
 =item turbidity
 
-Optional. The default is 180 ntu.
+Optional. The default threshold is C<180 ntu> and the default turbidity is set to C<10 ntu>.
 
 This will set the threshold of the turbidity of the water.
 
@@ -76,10 +74,81 @@ The range of values are between 0 and 300 ntu.
 
 =cut
 
-sub install {...}
+sub install {
+    my $class = shift;
+    my %sensors = @_;
+    
+    if ( not $sensors{pH} ) {
+        $sensors{pH_range} = [6.5, 7.5];
+        $sensors{current_pH} = 7.0;
+        $sensors{pH_LED_on} = 0;
+    }
+    
+    if ( not $sensors{temperature} ) {
+        $sensors{temperature_range} = [20, 25];
+        $sensors{current_temperature} = 23;
+        $sensors{temperature_LED_on} = 0;
+    }
+    
+    if ( not $sensors{DO} ) {
+        $sensors{DO_threshold} = 5;
+        $sensors{current_DO} = 8;
+        $sensors{DO_LED_on} = 0;
+    }
+    
+    if ( not $sensors{turbidity} ) {
+        $sensors{turbidity_threshold} = 180;
+        $sensors{current_turbidity} = 10;
+        $sensors{turbidity_LED_on} = 0;
+    }
+    
+    $sensors{lighted_LED_count} = 0;
+    $sensors{short_buzzer_on} = 0;
+    $sensors{long_buzzer_on} = 0;
+    
+    bless \%sensors, "Acme::FishFarm::WaterConditionMonitor";
+}
 
+
+=head2 add_oxygen_maintainer ( $oxygen_maintainer )
+
+Connects the oxygen maintainer to this monitoring system.
+
+For now, this module can only check if the oxygen is lacking or not. This module contains a user friendly method compared to the standard terminology used in the C<Acme::FishFarm::OxygenMaintainer> module. Other user friendly methods will be added in the future.
+
+=cut
+
+sub add_oxygen_maintainer {...}
 
 =head1 WATER CONDITIONS RELATED SUBROUTINES/METHODS
+
+=head2 current_ph ( $new_ph )
+
+Sets / returns the current pH of the water.
+
+C<$new_pH> is optional. If present, the current pH will be set to C<$new_ph>. Otherwise, returns the current pH reading.
+
+=cut
+
+sub current_ph {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    if ( @_ ) {
+        $self->{current_pH} = shift;
+    } else {
+        $self->{current_pH};
+    }
+}
+
+=head2 ph_threshold ()
+
+Returns the pH threshold as an array ref.
+
+=cut
+
+sub ph_threshold {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    $self->{pH_range};
+}
 
 =head2 set_ph_threshold ( $ph_value )
 
@@ -87,15 +156,73 @@ Sets the pH threshold.
 
 =cut
 
-sub set_ph_threshold {...}
+sub set_ph_threshold {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    my $new_ph_range_ref = shift;
+    croak "Please supply an array ref to set new pH range" if ref( $new_ph_range_ref ) ne ref( [] );
+    $self->{pH_range} = $new_ph_range_ref;
+}
 
 =head2 ph_is_normal ()
 
 Returns true if the current pH is within the set range of threshold.
 
+The pH LED will light up and a short buzzer will be turned on if the pH is not normal.
+
+The long buzzer will not be affected, you'll need to turn it on yourself.
 =cut
 
-sub ph_is_normal {...}
+sub ph_is_normal {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    
+    if ( $self->{current_pH} >= $self->{pH_range}[0] and $self->{current_pH} <= $self->{pH_range}[1] ) {
+    
+        # if still on, switch it off, it's normal now
+        if ( $self->{lighted_LED_count} == 1 and $self->is_on_LED_pH ) {
+            $self->{pH_LED_on} = 0;
+            $self->{lighted_LED_count}--;
+            $self->{short_buzzer_on} = 0 ; # the only one lighting up the LED
+            $self->{long_buzzer_on} = 0 if $self->{lighted_LED_count} < 2;
+        }
+    
+        return 1;
+    } else {
+        $self->{pH_LED_on} = 1;
+        $self->{lighted_LED_count}++;
+        $self->{short_buzzer_on} = 1;
+        return 0;
+    }
+}
+
+=head2 current_temperature ( $new_temperature )
+
+Sets / returns the current temperature of the water.
+
+C<$new_temperature> is optional. If present, the current temperature will be set to C<$new_temperature>. Otherwise, returns the current temperature reading.
+
+=cut
+
+sub current_temperature {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    
+    if ( @_ ) {
+        $self->{current_temperature} = shift;
+    } else {
+        $self->{current_temperature};
+    }
+    
+}
+
+=head2 temperature_threshold ()
+
+Returns the acceptable temperature range as an array ref.
+
+=cut
+
+sub temperature_threshold {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    $self->{temperature_range};
+}
 
 =head2 set_temperature_threshold ( $celcius )
 
@@ -103,7 +230,13 @@ Sets the water temperature threshold.
 
 =cut
 
-sub set_temperature_threshold {...}
+sub set_temperature_threshold {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    my $new_temperature_range_ref = shift;
+    croak "Please supply an array ref to set new temperature range" 
+        if ref( $new_temperature_range_ref ) ne ref( [] );
+    $self->{temperature_range} = $new_temperature_range_ref;
+}
 
 =head2 temperature_is_normal ()
 
@@ -111,7 +244,28 @@ Returns true if the current temperature is within the set range of threshold.
 
 =cut
 
-sub temperature_is_normal {...}
+sub temperature_is_normal {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    
+    if ( $self->{current_temperature} >= $self->{temperature_range}[0] 
+        and $self->{current_temperature} <= $self->{temperature_range}[1] ) {
+        
+        # if still on, switch it off, it's normal now
+        if ( $self->{lighted_LED_count} == 1 and $self->is_on_LED_temperature ) {
+            $self->{temperature_LED_on} = 0;
+            $self->{lighted_LED_count}--;
+            $self->{short_buzzer_on} = 0 ; # the only one lighting up the LED
+            $self->{long_buzzer_on} = 0 if $self->{lighted_LED_count} < 2;
+        }
+        
+        return 1;
+    } else {
+        $self->{temperature_LED_on} = 1;
+        $self->{lighted_LED_count}++;
+        $self->{short_buzzer_on} = 1;
+        return 0;
+    }
+}
 
 =head2 lacking_oxygen ()
 
@@ -120,6 +274,24 @@ Returns true if the current DO content is lower then the threshold.
 =cut
 
 sub lacking_oxygen {...}
+
+=head2 current_turbidity ( $new_turbidity )
+
+Sets / returns the current turbidity of the water.
+
+C<$new_turbidity> is optional. If present, the current turbidity will be set to C<$new_turbidity>. Otherwise, returns the current turbidity reading.
+
+=cut
+
+sub current_turbidity {...}
+
+=head2 turbidity_threshold
+
+Returns the turbidity threshold.
+
+=head2 set_turbidity_threshold ( $new_turbidity_threshold )
+
+Sets the turbidity threshold to C<$new_turbidity_threshold>.
 
 =head2 water_dirty ()
 
@@ -133,23 +305,54 @@ sub water_dirty {...}
 # these 2 should be wrappers of something in the future
 =head1 BUZZER RELATED SUBROUTINES/METHODS
 
-=head2 buzz_short ()
+=head2 on_buzzer_short ()
 
 Makes the buzzer buzz for a while if only one LED is lighted up.
 
 =cut
 
-sub buzz_short {...}
+sub on_buzzer_short {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    $self->{short_buzzer_on} = 1;
+}
 
-=head2 buzz_long ()
+=head2 is_on_buzzer_short ()
+
+Returns true if the short buzzer is turned on.
+
+=cut
+
+sub is_on_buzzer_short {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    $self->{short_buzzer_on};
+}
+
+=head2 on_buzzer_long ()
 
 Makes the buzzer buzz infinitely until less than 2 LEDs are lighted up.
 
 =cut
 
-sub buzz_long {...}
+sub on_buzzer_long {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    $self->{long_buzzer_on} = 1;
+}
 
+=head2 is_on_buzzer_long ()
 
+Returns true if the long buzzer is turned on.
+
+=cut
+
+sub is_on_buzzer_long {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    if ( $self->{lighted_LED_count} > 1 ) {
+        $self->{long_buzzer_on} = 1; # this seems pretty useless
+        1;
+    } else {
+        0;
+    }
+}
 
 =head1 LED LIGHTS RELATED SUBROUTINES/METHODS
 
@@ -158,7 +361,7 @@ This action is to be done in through the main script.
 
 =head2 on_LED_pH ()
 
-Light up the LED for pH sensor, indicating abnormal pH.
+Lights up the LED for pH sensor, indicating abnormal pH.
 
 =head2 is_on_LED_pH ()
 
@@ -166,13 +369,19 @@ Returns true if the LED of pH is lighted up.
 
 =cut
 
-sub on_LED_pH {...}
+sub on_LED_pH {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    $self->{pH_LED_on} = 1;
+}
 
-sub is_on_LED_pH {...}
+sub is_on_LED_pH {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    return $self->{pH_LED_on};
+}
 
 =head2 on_LED_temperature ()
 
-Light up the LED for temperature sensor, indicating abnormal water temperature.
+Lights up the LED for temperature sensor, indicating abnormal water temperature.
 
 =head2 is_on_LED_temperature ()
 
@@ -180,13 +389,21 @@ Returns true if the LED of temperature is lighted up.
 
 =cut
 
-sub on_LED_temperature {...}
+sub on_LED_temperature {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    $self->{temperature_LED_on} = 1;
+}
 
-sub is_on_LED_temperature {...}
+sub is_on_LED_temperature {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    return $self->{temperature_LED_on};
+}
 
 =head2 on_LED_DO ()
 
-Light up the LED for dissolved oxygen sensor, indicating low DO content. Fish might die :)
+Lights up the LED for dissolved oxygen sensor, indicating low DO content. Fish might die :)
+
+Take note that this will interact with the actual oxygen maintainer system.
 
 =head2 is_on_LED_DO ()
 
@@ -211,6 +428,17 @@ Returns true if the LED of DO is lighted up.
 sub on_LED_turbidity {...}
 
 sub is_on_LED_turbidity {...}
+
+=head2 lighted_LED_count ()
+
+Returns the number of LEDs lighted up currently
+
+=cut
+
+sub lighted_LED_count {
+    ref( my $self = shift ) or croak "Please use this the OO way";
+    $self->{lighted_LED_count};
+}
 
 =head1 AUTHOR
 
--- t/10-water_condition_monitor.t
+++ t/10-water_condition_monitor.t
@@ -8,6 +8,64 @@ BEGIN {
     use_ok( "Acme::FishFarm::WaterConditionMonitor" ) || BAIL_OUT;
 }
 
+my $water_monitor = Acme::FishFarm::WaterConditionMonitor->install();
+
+is( ref($water_monitor), "Acme::FishFarm::WaterConditionMonitor", "Correct class" );
+
+# pH
+my $ph_range =  $water_monitor->ph_threshold;
+is( $ph_range->[0], 6.5, "Correct min pH" );
+is( $ph_range->[1], 7.5, "Correct max pH" );
+
+ok($water_monitor->set_ph_threshold([4, 7]), "New pH can be set");
+my $new_ph_range =  $water_monitor->ph_threshold;
+is( $new_ph_range->[0], 4, "Correct new min pH" );
+is( $new_ph_range->[1], 7, "Correct new max pH" );
+
+my $current_ph = $water_monitor->current_ph;
+is( $current_ph, 7, "Correct default pH value");
+is( $water_monitor->ph_is_normal, 1, "pH level is normal" );
+isnt( $water_monitor->is_on_LED_pH, 1, "pH LED is not on" );
+is( $water_monitor->is_on_buzzer_short, 0, "pH sensor not making short buzzer turn on" );
+
+# temperature
+my $temperature_range =  $water_monitor->temperature_threshold;
+is( $temperature_range->[0], 20, "Correct min temperature" );
+is( $temperature_range->[1], 25, "Correct max temperature" );
+
+ok($water_monitor->set_temperature_threshold([15, 20]), "New pH can be set");
+my $new_temperature_range =  $water_monitor->temperature_threshold;
+is( $new_temperature_range->[0], 15, "Correct new min temperature" );
+is( $new_temperature_range->[1], 20, "Correct new max temperature" );
+
+my $current_temperature = $water_monitor->current_temperature;
+is( $current_temperature, 23, "Correct default temperature value");
+isnt( $water_monitor->temperature_is_normal, 1, "Temperature is not normal right now" );
+is( $water_monitor->is_on_LED_temperature, 1, "Temperature LED is on" );
+is( $water_monitor->is_on_buzzer_short, 1, "Temperature sensor is turning on short buzzer" );
+
+isnt ( $water_monitor->is_on_buzzer_long , 1, "Long buzzer is not switched on yet, nice");
+
+print "Short buzzer on: ", $water_monitor->{short_buzzer_on}, "\n";
+print "Long buzzer on: ", $water_monitor->{long_buzzer_on}, "\n";
+is( $water_monitor->lighted_LED_count, 1, "1 LED lighted up" );
+
+# try to make the long buzzer switch on and off again
+# must check for normal state before checking the LEDs and buzzers
+$water_monitor->current_ph(8);
+is( $water_monitor->current_ph, 8, "Correct new pH value" );
+$water_monitor->ph_is_normal;
+is( $water_monitor->is_on_LED_pH, 1, "pH LED is on" );
+is( $water_monitor->is_on_buzzer_short, 1, "pH sensor causing short buzzer to turn on" );
+
+print "Short buzzer on: ", $water_monitor->{short_buzzer_on}, "\n";
+print "Long buzzer on: ", $water_monitor->{long_buzzer_on}, "\n";
+is( $water_monitor->lighted_LED_count, 2, "2 LEDs lighted up" );
+
+# temperature and pH both not normal, long buzzer should go off
+is( $water_monitor->is_on_buzzer_long, 1, "pH and temperature caused long buzzer to go off" );
+
+
 
 done_testing;
 
