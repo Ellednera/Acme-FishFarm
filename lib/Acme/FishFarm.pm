6bd706fdea796f978514552af4279b93eb8e22ca
Ellednera <raphael.jongjj@gmail.com>
2021-10-27 21:56:13 +0800

    Fish farm half done, tests passed

--- lib/Acme/FishFarm.pm
+++ lib/Acme/FishFarm.pm
@@ -3,10 +3,15 @@ package Acme::FishFarm;
 use 5.006;
 use strict;
 use warnings;
+use Carp "croak";
+
+use Acme::FishFarm::Feeder;
+use Acme::FishFarm::OxygenMaintainer;
+use Acme::FishFarm::WaterConditionMonitor;
 
 =head1 NAME
 
-Acme::FishFarm - A fish farm that has automated systems
+Acme::FishFarm - A fish farm with automated systems
 
 =head1 VERSION
 
@@ -24,130 +29,264 @@ our $VERSION = '0.01';
 
 =head1 EXPORT
 
-None by default
-
-
-=head1 CREATION RELATED METHODS
-
-=head2 new ()
-
-Creates a new fish farm.
+The tag C<:all> can be used to import all the functions available in this module.
 
 =cut
 
-sub new {...}
+use Exporter qw( import );
+our @EXPORT_OK = qw( 
+    install_all_systems 
+    reduce_precision consume_oxygen 
+    check_DO check_pH check_temperature check_turbidity
+    render_leds render_buzzer
+);
+our %EXPORT_TAGS = ( 
+    all => [ qw( install_all_systems reduce_precision consume_oxygen check_DO check_pH check_temperature 
+                 check_turbidity render_leds render_buzzer ) ],
+);
 
-=head2 install ( systems => [...] )
+=head1 SYSTEM INSTALLATION RELATED SUBROUTINES
 
-Installs the specified systems to your fish farm. This is done by setting the C<systems> key.
+=head2 install_all_systems
 
-The values for C<systems> include:
+Installs all the available systems with default values and returns them as a list of C<Acme::FishFarm::*> objects in the following sequence:
 
-=over 4
-
-=item * water_level
-
-=item * feeder
-
-=item * water_filter
-
-=item * water_monitor
-
-#=item * oxygen_level
-
-=back
-
-To install all the systems in one go, use the C<install_all_systems> method instead.
+    C<(Feeder, OxygenMaintainer, WaterConditionMonitor, WaterLevelMaintainer, WaterFiltration)>
 
 =cut
 
-sub install {...}
-
-=head2 install_all_systems ()
-
-Installs all the systems available in the fish farm. See C<install> method for the available systems
-
-=cut
-
-sub install_all_systems {...}
-
-
-=head1 WATER LEVEL MAINTAINER SUBROUTINES/METHODS
-
-=head2 check_water_level ()
-
-Returns the current water level in your pond, tank, aquarium or whatever.
-
-=cut
-
-sub check_water_level {...}
+sub install_all_systems {
+    my $feeder = Acme::FishFarm::Feeder->install;
+    my $oxygen_maintainer = Acme::FishFarm::OxygenMaintainer->install;
+    my $water_monitor = Acme::FishFarm::WaterConditionMonitor->install;
+    my $water_level = "";
+    my $water_filter = "";
+    
+    # remember to connect water oxygem maintainer to water condition monitoring :)
+    $water_monitor->add_oxygen_maintainer( $oxygen_maintainer );
+    
+    ( $feeder, $oxygen_maintainer, $water_monitor, $water_level, $water_filter );
+}
 
 
-=head1 AUTOMATED FEEDER SUBROUTINES/METHODS
+=head1 SENSOR READING RELATED SUBROUTINES
 
-=head2 check_feeder () ??? maybe for refilling :)
+=head2 reduce_precision ( $decimal )
 
-Returns the amount (volume) of fish food in the feeder. 
+Reduces C<$decimal> to a maximum of 3 decimal points. Make sure to pass in a decimal with more than 3 decimal points.
 
-This is only useful if you want to see how much fish food is still there. This is not an important feature of this module
+Returns the reduced 3-decimal value.
+
+This subroutine is useful if you are trying to set the current sensor readings randomly using the built-in C<rand> function as you do not want to end up with too many decimals on the screen.
 
 =cut
 
-sub check_feeder {...}
+sub reduce_precision {
+    my $sensor_reading = shift;
+    croak "Please pass in a decimal value" if not $sensor_reading =~ /\./;
+    $sensor_reading =~ /(\d+\.\d{3})/;
+    $1;
+}
 
+=head1 AUTOMATED SYSTEMS RELATED SUBROUTINES
 
-=head1 WATER FILTRATION SUBROUTINES/METHODS
+All of the subroutines here will give output.
 
-=head2 check_water_filter () ??? maybe just to inspect for fun :)
+=head2 consume_oxygen ( $oxygen_maintainer, $consumed_oxygen )
 
-Returns the amount of waste in the filtering cylinder.
+This will cause the oxygen level (DO level) of the C<Acme::FishFarm::OxygenMaintainer> to reduce by C<$consumed_oxygen mg/L>
+
+Returns 1 upon success.
+=cut
+
+sub consume_oxygen {
+    my $o2_maintainer = shift;
+    my $consumed_oxygen = shift;
+    print "$consumed_oxygen mg/L of oxygen consumed...\n";
+    my $o2_remaining = $o2_maintainer->current_DO - $consumed_oxygen;
+    $o2_maintainer->current_DO( $o2_remaining );
+    1;
+}
+
+=head2 check_DO ( $oxygen_maintainer, $current_DO_reading )
+
+This checks and outputs the condition of the current DO level.
+
+Take note that this process will also trigger the LED and buzzer if abnormal condition is present.
+
+Returns 1 upon success.
+=cut
+
+sub check_DO {
+    my ( $oxygen, $current_reading ) = @_;
+    my $DO_threshold = $oxygen->DO_threshold;
+
+    $oxygen->current_DO( $current_reading );
+    $oxygen->is_low_DO;
+    
+    print "Current Oxygen Level: ", $current_reading, " mg/L",
+        " (low: < ", $DO_threshold, ")\n";
+
+    if ( $oxygen->is_low_DO ) {
+        print "  !! Low oxygen level!\n";
+    } else {
+        print "  Oxygen level is normal.\n";
+    }
+    1;
+}
+
+=head2 check_pH ( $water_monitor, $current_ph_reading )
+
+This checks and outputs the condition of the current pH value.
+
+Take note that this process will also trigger the LED and buzzer if abnormal condition is present.
+
+Returns 1 upon success.
 
 =cut
 
-sub check_water_filter {...}
+sub check_pH {
+    my ( $water_monitor, $current_reading ) = @_;
+    my $ph_range = $water_monitor->ph_threshold;
+    
+    $water_monitor->current_ph( $current_reading );
+    $water_monitor->ph_is_normal;
+    
+    print "Current pH: ", $water_monitor->current_ph, 
+        " (normal range: ", $ph_range->[0], "~", $ph_range->[1], ")\n";
 
+    if ( !$water_monitor->ph_is_normal ) {
+        print "  !! Abnormal pH!\n";
+    } else {
+        print "  pH is normal.\n";
+    }
+    1;
+}
 
-=head1 WATER CONDITION MONITORING SUBROUTINES/METHODS
+=head2 check_temperature ( $water_monitor, $current_temperature_reading )
 
-=head2 check_water_monitor ()
+This checks and outputs the condition of the current temperature.
 
-Returns a hash of the current condition of the water. The hash will include the following keys:
+Take note that this process will also trigger the LED and buzzer if abnormal condition is present.
 
-=over 4
-
-=item 1. pH
-
-The values are in the range of 1-14
-
-=item 2. temperature
-
-The ranges of values are between 0 and 50 degree B<celciuls>
-
-=item 3. DO
-
-The range of values are between 0 and 10 mg/L
-
-=item 4. turbidity
-
-The range of values are between 0 and 300 ntu.
-
-=back
+Returns 1 upon success.
 
 =cut
 
-sub check_water_monitor {...}
+sub check_temperature {
+    my ( $water_monitor, $current_reading ) = @_;
+    my $temperature_range = $water_monitor->temperature_threshold;
 
-=head1 OXYGEN LEVEL MAINTAINER SUBROUTINES/METHODS
+    $water_monitor->current_temperature( $current_reading );
+    $water_monitor->temperature_is_normal;
+    
+    print "Current temperature: ", $water_monitor->current_temperature, " C", 
+        " (normal range: ", $temperature_range->[0], " C ~ ", $temperature_range->[1], " C)\n";
 
-=head2 check_oxygen_level ()
+    if ( !$water_monitor->temperature_is_normal ) {
+        print "  !! Abnormal temperature!\n";
+    } else {
+        print "  Temperature is normal.\n";
+    }
+    1;
+}
 
-This is the same as checking the DO content through the C<check_water_filter> method.
+=head2 check_turbidity ( $water_monitor, $current_turbidity_reading )
 
-Either this part of the DO part might be removed
+This checks and outputs the condition of the current temperature.
+
+Take note that this process will also trigger the LED and buzzer if abnormal condition is present.
+
+Returns 1 upon success.
 
 =cut
 
-sub check_oxygen_level {...}
+sub check_turbidity {
+    my ( $water_monitor, $current_reading ) = @_;
+    my $turbidity_threshold = $water_monitor->turbidity_threshold;
+
+    $water_monitor->current_turbidity( $current_reading );
+    
+    print "Current Turbidity: ", $water_monitor->current_turbidity, " ntu",
+        " (dirty: > ", $turbidity_threshold, ")";
+
+    if ( $water_monitor->water_dirty ) {
+        print "  !! Water is dirty!\n";
+    } else {
+        print "  Water is still clean.\n";
+    }
+    1;
+}
+
+=head2 render_leds ( $water_monitor )
+
+Outputs which LEDs are lighted up. Returns 1 upon success.
+
+Currently this subroutine only shows the LEDs present in C<$water_monitor> ie. C<Acme::FishFarm::WaterConditionMonitor> object. See that module for more details about the available LEDs.
+
+More LEDs will be available in the future. You can append your own LEDs by yourself if you really need to :)
+
+=cut
+
+sub render_leds {
+    my $water_monitor = shift;
+
+    # must check condition first! If not it won't work
+        # this process will update the LEDs status
+    $water_monitor->ph_is_normal;
+    $water_monitor->temperature_is_normal;
+    $water_monitor->lacking_oxygen;    
+    $water_monitor->water_dirty;
+        
+    print "Total LEDs up: ", $water_monitor->lighted_LED_count, "\n";
+
+    if ( $water_monitor->is_on_LED_pH ) {
+        print "  pH LED: on\n";
+    } else {
+        print "  pH LED: off\n";
+    }
+    
+    if ( $water_monitor->is_on_LED_temperature ) {
+        print "  Temperature LED: on\n";
+    } else {
+        print "  Temperature LED: off\n";
+    }
+    
+    if ( $water_monitor->is_on_LED_DO ) {
+        print "  Low DO LED: on\n";
+    } else {
+        print "  Low DO LED: off\n";
+    }
+    
+    if ( $water_monitor->is_on_LED_turbidity ) {
+        print "  Turbidity LED: on\n";
+    } else {
+        print "  Turbidity LED: off\n";
+    }
+    1;
+}
+
+=head2 render_buzzer ( $water_monitor )
+
+Outputs which buzzer is buzzing. Returns 1 upon success.
+
+See C<Acme::FishFarm::WaterConditionMonitor> for details on how the short and long buzzers are switched on and off.
+
+=cut
+
+sub render_buzzer {
+    my $water_monitor = shift;
+    
+    #$water_monitor->_tweak_buzzers;
+    if ( $water_monitor->is_on_buzzer_long ) {
+        print "Long buzzer is on, water condition very critical!\n";
+    } elsif ( $water_monitor->is_on_buzzer_short ) {
+        print "Short buzzer is on, water condition is a bit worrying :|\n";
+    } else {
+        print "No buzzers on, what a peaceful moment.\n";
+    }
+    1;
+}
 
 =head1 AUTHOR
 
--- t/02-fish_farm.t
+++ t/02-fish_farm.t
@@ -4,15 +4,35 @@ use strict;
 use warnings;
 use Test::More;
 
-BEGIN {
-    use_ok( "Acme::FishFarm" ) || BAIL_OUT;
-}
+use Acme::FishFarm ":all";
 
 
+my ($feeder, $oxygen, $water_monitor, $water_level, $water_filter ) = 
+    Acme::FishFarm->install_all_systems;
+
+is( ref($feeder), "Acme::FishFarm::Feeder", "Correct feeder" );
+is( ref($oxygen), "Acme::FishFarm::OxygenMaintainer", "Correct oxygem maintainer" );
+is( ref($water_monitor), "Acme::FishFarm::WaterConditionMonitor", "Correct water condition monitor" );
+#is( ref($water_level), "Acme::FishFarm::WaterLevelMaintainer", "Correct water level monitor" );
+#is( ref($water_filter), "Acme::FishFarm::WaterFiltration", "Correct water filter" );
+
+is( reduce_precision(3.14159), 3.141, "reduce_precision is working correctly" );
+ok( consume_oxygen( $oxygen, 2.5 ), "consume_oxygen is working correctly");
+ok( consume_oxygen( $oxygen, 2 ), "consume_oxygen is working correctly");
+
+ok( check_DO( $oxygen, 5 ), "check_DO is working correctly" );
+ok( check_pH( $water_monitor, 5 ), "check_pH is working correctly" );
+ok( check_temperature( $water_monitor, 25 ), "check_temperature is working correctly" );
+ok( check_turbidity( $water_monitor, 250 ), "check_turbidity is working correctly" );
+
+
+ok( render_leds( $water_monitor), "render_leds is working correctly" );
+ok( render_buzzer( $water_monitor ), "render_buzzer is working correctly" );
 
 done_testing;
 
 # besiyata d'shmaya
+
 
 
 
